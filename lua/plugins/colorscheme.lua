cffc968275b64a913e9fcb144385306e676c9909
Pedro Rezende <xpedrorezende@gmail.com>
2024-02-03 02:28:12 -0300

    feat: installing cool themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "arcticicestudio/nord-vim", name = "nord" },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "cocopon/iceberg.vim", name = "iceberg" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-frappe",
+      colorscheme = "iceberg",
     },
   },
 }
